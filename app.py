70028a9e3999ee56e84688e40fc65a5d78908027
anarshah <javacodesmaster@gmail.com>
2023-04-07 00:44:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,59 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import joblib
 
-def load_data():
-    data = pd.read_csv("zameen-property-data.csv")
-    data.drop(["property_id", "location_id", "page_url", "latitude", "longitude", "date_added", "agency", "agent"], axis=1, inplace=True)
-    return data
+# Load the trained model
+model = joblib.load('model.joblib')
 
-def preprocess_data(data):
-    # encode categorical features
-    encoder = LabelEncoder()
-    data["property_type"] = encoder.fit_transform(data["property_type"])
-    data["location"] = encoder.fit_transform(data["location"])
-    data["city"] = encoder.fit_transform(data["city"])
-    data["province_name"] = encoder.fit_transform(data["province_name"])
-    data["purpose"] = encoder.fit_transform(data["purpose"])
-    
-    # split data into X and y
-    X = data.drop(["price"], axis=1)
-    y = data["price"]
-    
-    # split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    return X_train, X_test, y_train, y_test
+# Load the data
+df = pd.read_csv('zameen-property-data.csv')
 
-def train_model(X_train, y_train):
-    # train a random forest regressor
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
-    return model
+# Remove unwanted columns
+df.drop(['property_id', 'location_id', 'page_url', 'purpose', 'date_added', 'agency', 'agent'], axis=1, inplace=True)
 
-def evaluate_model(model, X_test, y_test):
-    # make predictions on test data
-    y_pred = model.predict(X_test)
-    
-    # calculate evaluation metrics
-    mae = mean_absolute_error(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
-    r2 = r2_score(y_test, y_pred)
-    
-    return mae, rmse, r2
+# Convert area column to numeric
+df['area'] = df['area'].str.replace('Marla', '').astype(float)
 
-def main():
-    st.set_page_config(page_title="Real Estate Price Prediction", page_icon=":house:", layout="wide")
-    st.title("Real Estate Price Prediction")
-    
-    # load data
-    data = load_data()
-    
-    # preprocess data
-    X_train, X_test, y_train, y_test = preprocess_data(data)
-    
-    # train model
-    model = train_model(X_train, y_train)
-    
-    # evaluate model
-    mae, rmse, r2 = evaluate_model(model, X_test, y_test)
-    
-    # display results
-    st.write("Mean Absolute Error:", mae)
-    st.write("Root Mean Squared Error:", rmse)
-    st.write("R2 Score:", r2)
+# Convert location column to categorical
+df['location'] = pd.Categorical(df['location']).codes
 
-if __name__ == "__main__":
-    main()
+# Convert city column to categorical
+df['city'] = pd.Categorical(df['city']).codes
+
+# Convert province_name column to categorical
+df['province_name'] = pd.Categorical(df['province_name']).codes
+
+# Convert property_type column to categorical
+df['property_type'] = pd.Categorical(df['property_type']).codes
+
+# Define a function to make a prediction
+def predict_price(bedrooms, bathrooms, area):
+    # Create a dictionary with the user input
+    data = {'bedrooms': bedrooms, 'bathrooms': bathrooms, 'area': area}
+    
+    # Create a DataFrame from the dictionary
+    df_user_input = pd.DataFrame(data, index=[0])
+    
+    # Use the trained model to make a prediction on the user input
+    predicted_price = model.predict(df_user_input)[0]
+    
+    return predicted_price
+
+# Define the Streamlit app
+def app():
+    st.title("Property Price Predictor")
+    
+    # Get user input
+    bedrooms = st.number_input("Enter number of bedrooms", min_value=1, max_value=10, step=1)
+    bathrooms = st.number_input("Enter number of bathrooms", min_value=1, max_value=10, step=1)
+    area = st.number_input("Enter area in square meters", min_value=1.0, max_value=1000.0, step=1.0)
+    
+    # Make a prediction
+    predicted_price = predict_price(bedrooms, bathrooms, area)
+    
+    # Show the predicted price
+    st.write(f"The predicted price is {predicted_price:.2f} PKR.")
+    
+# Run the app
+if __name__ == '__main__':
+    app()
