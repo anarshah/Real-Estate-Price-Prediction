9e4dab87fb39e6a5f39c33a5ada7ab17c6d158ba
anarshah <javacodesmaster@gmail.com>
2023-04-07 18:34:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
 import streamlit as st
-import joblib
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeRegressor
+
 
 # Load the trained decision tree model
-decision_tree_model = joblib.load('decision_tree_model.joblib')
+model = joblib.load('decision_tree_model.joblib')
 
-# Load the LabelEncoder
+# Load the label encoder for the location feature
 le = joblib.load('label_encoder.joblib')
 
-# Create a list of available locations
-locations = ['Bahria Town', 'DHA Defence', 'Gulberg', 'Johar Town', 'Wapda Town']
 
-# Get the user input
-st.title("Real Estate Price Predictor")
-location = st.selectbox("Select Location", locations)
-sqft = st.number_input("Enter Square Feet")
-bedrooms = st.number_input("Enter Number of Bedrooms")
-bathrooms = st.number_input("Enter Number of Bathrooms")
- 
-# Transform location using the LabelEncoder
-le.transform(locations)
+# Define a function to preprocess the input data and make predictions
+def predict_price(location, sqft, bedrooms, bathrooms):
+    # Encode the location feature
+    location_encoded = le.transform([location])[0]
 
-# Make the prediction
-location_encoded = le.transform([location])[0]
-prediction = decision_tree_model.predict([[location_encoded, sqft, bedrooms, bathrooms]])
+    # Create a dictionary with the input data
+    input_dict = {
+        'location': location_encoded,
+        'total_sqft': sqft,
+        'bedrooms': bedrooms,
+        'bathrooms': bathrooms
+    }
 
-# Display the prediction
-st.write("The estimated price of the property is", prediction[0])
+    # Create a pandas DataFrame from the input dictionary
+    input_df = pd.DataFrame([input_dict])
+
+    # Fit the model on the input data
+    model.fit(X_train, y_train)
+
+    # Make predictions on the input data
+    prediction = model.predict(input_df)
+
+    return prediction
+
+
+# Create the Streamlit app
+st.title('Real Estate Price Prediction')
+
+# Add input fields for the user to enter the property details
+location = st.selectbox('Location', ['Gulberg', 'DHA', 'Bahria Town', 'Safari Villas'])
+sqft = st.number_input('Total sqft')
+bedrooms = st.number_input('Bedrooms')
+bathrooms = st.number_input('Bathrooms')
+
+# Add a button to make predictions on the input data
+if st.button('Predict'):
+    # Call the predict_price function with the input data
+    prediction = predict_price(location, sqft, bedrooms, bathrooms)
+    st.success(f'The predicted price is {prediction:.2f} PKR.')
