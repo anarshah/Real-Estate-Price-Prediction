66bd6c3cf6cd90652daeb77d58ade7a51a86cb0a
anarshah <javacodesmaster@gmail.com>
2023-04-07 18:29:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import streamlit as st
 import joblib
+import numpy as np
 
-# Load the saved model
-model = joblib.load('decision_tree_model.joblib')
+# Load the saved decision tree model
+decision_tree_model = joblib.load('decision_tree_model.joblib')
 
-# Create a function to take input and make predictions
-def predict_price(feature_vector):
-    # Reshape the feature vector to match the expected input shape of the model
-    feature_vector = feature_vector.reshape(1, -1)
-    # Make a prediction using the loaded model
-    prediction = model.predict(feature_vector)
-    return prediction[0]
+# Create a form for user input
+st.write('Please enter the following information:')
+form = st.form(key='prediction_form')
+beds = form.number_input('Number of bedrooms:', min_value=1, max_value=10, value=2)
+baths = form.number_input('Number of bathrooms:', min_value=1, max_value=10, value=2)
+area = form.number_input('Area (in square feet):', min_value=1, value=1000)
+location = form.selectbox('Location:', ['Gulberg', 'DHA', 'Bahria Town', 'Model Town'])
+submit_button = form.form_submit_button(label='Get Prediction')
 
-# Create a Streamlit app
-def app():
-    # Create a form to take input from the user
-    st.write("Enter the features of the property:")
-    area = st.number_input("Area (in square feet)")
-    bedrooms = st.number_input("Number of bedrooms")
-    bathrooms = st.number_input("Number of bathrooms")
-    location = st.text_input("Location")
-    # Convert the user input to a feature vector
-    feature_vector = [area, bedrooms, bathrooms, location]
-    # Make a prediction using the loaded model and the user input
-    prediction = predict_price(feature_vector)
+# Make a prediction based on user input
+if submit_button:
+    # Encode the location using the same LabelEncoder as used during training
+    le = joblib.load('label_encoder.joblib')
+    location_encoded = le.transform([location])[0]
+
+    # Create a numpy array from user input and location encoded value
+    input_features = np.array([[beds, baths, area, location_encoded]])
+
+    # Make a prediction using the saved decision tree model
+    prediction = decision_tree_model.predict(input_features)
+
     # Display the prediction to the user
-    st.write("The estimated price of the property is:", prediction)
+    st.write('The predicted price is: ${:.2f}'.format(prediction[0]))
