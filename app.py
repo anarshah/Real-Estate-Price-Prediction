9ce1001863f5e20ae4f9cab19fa83c587dcb2469
anarshah <javacodesmaster@gmail.com>
2023-04-07 19:01:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,20 @@
-import streamlit as st
-import pandas as pd
-import joblib
-from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeRegressor
+import joblib
 
-# Load the trained decision tree model
+# load the saved model and label encoder
 model = joblib.load('decision_tree_model.joblib')
+encoder = joblib.load('label_encoder.joblib')
 
-# Load the label encoder for the location feature
-le = joblib.load('label_encoder.joblib')
-
-# Define a function to preprocess the input data and make predictions
+# define a function to predict the price
 def predict_price(location, sqft, bedrooms, bathrooms):
-    # Encode the location feature
-    location_encoded = le.transform([location])[0]
+    # encode the location using the label encoder
+    location_encoded = encoder.transform([location])[0]
 
-    # Create a dictionary with the input data
-    input_dict = {
-        'location': location_encoded,
-        'total_sqft': sqft,
-        'bedrooms': bedrooms,
-        'bathrooms': bathrooms
-    }
+    # create a data frame with the input features
+    input_data = [[location_encoded, sqft, bedrooms, bathrooms]]
+    input_df = pd.DataFrame(input_data, columns=['location_encoded', 'sqft', 'bedrooms', 'bathrooms'])
 
-    # Create a pandas DataFrame from the input dictionary
-    input_df = pd.DataFrame([input_dict])
-
-    # Make predictions on the input data
+    # predict the price using the fitted model
     prediction = model.predict(input_df)
 
     return prediction
-
-# Create the Streamlit app
-st.title('Real Estate Price Prediction')
-
-# Add input fields for the user to enter the property details
-location = st.selectbox('Location', ['Gulberg', 'DHA', 'Bahria Town', 'Safari Villas'])
-sqft = st.number_input('Total sqft')
-bedrooms = st.number_input('Bedrooms')
-bathrooms = st.number_input('Bathrooms')
-
-# Add a button to make predictions on the input data
-if st.button('Predict'):
-    # Call the predict_price function with the input data
-    prediction = predict_price(location, sqft, bedrooms, bathrooms)
-    st.success(f'The predicted price is {prediction:.2f} PKR.')
