058d0926638fff8a94265939bcfc8cda23e2140f
anarshah <javacodesmaster@gmail.com>
2023-04-07 19:44:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import streamlit as st
+import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 import joblib
 
@@ -18,3 +20,18 @@ def predict_price(location, sqft, bedrooms, bathrooms):
     prediction = model.predict(input_df)
 
     return prediction
+
+# set up the Streamlit app
+st.set_page_config(page_title='Real Estate Price Prediction', page_icon=':money_with_wings:')
+st.title('Real Estate Price Prediction')
+
+# define the input fields
+location = st.selectbox('Location', ['Gulberg', 'DHA', 'Bahria Town', 'Model Town', 'Johar Town'])
+sqft = st.number_input('Square Feet')
+bedrooms = st.number_input('Bedrooms')
+bathrooms = st.number_input('Bathrooms')
+
+# make the prediction when the 'Predict' button is clicked
+if st.button('Predict'):
+    prediction = predict_price(location, sqft, bedrooms, bathrooms)
+    st.success(f'The predicted price is {prediction[0]:,.0f} PKR.')
