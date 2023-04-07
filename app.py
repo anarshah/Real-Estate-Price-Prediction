f7c40216c05b77a05d77d52e0b1b8e00efd48244
anarshah <javacodesmaster@gmail.com>
2023-04-07 12:03:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,65 @@
 import streamlit as st
 import pandas as pd
-import joblib
+import numpy as np
+import re
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.externals import joblib
 
-# Load the model
+
+# Load the trained model
 model = joblib.load('model.joblib')
 
-# Define the input fields
-st.sidebar.header('Property Information')
-property_type = st.sidebar.selectbox('Property Type', ['House', 'Apartment', 'Condo'])
-location = st.sidebar.text_input('Location')
-city = st.sidebar.selectbox('City', ['Toronto', 'Vancouver', 'Montreal'])
-province_name = st.sidebar.selectbox('Province', ['Ontario', 'British Columbia', 'Quebec'])
-latitude = st.sidebar.number_input('Latitude')
-longitude = st.sidebar.number_input('Longitude')
-baths = st.sidebar.number_input('Number of Bathrooms', value=1)
-area = st.sidebar.number_input('Area (in sqft)', value=500)
-bedrooms = st.sidebar.number_input('Number of Bedrooms', value=1)
+# Define a function to preprocess the input data
+def preprocess_input(baths, bedrooms, area, location, city, province_name):
+    # Preprocess the area feature
+    pattern = r'^([\d\.]+)'
+    area_num = float(re.findall(pattern, area)[0])
+    
+    # Create a DataFrame with the preprocessed input data
+    input_df = pd.DataFrame({
+        'baths': [baths],
+        'bedrooms': [bedrooms],
+        'area_num': [area_num],
+        'location': [location],
+        'city': [city],
+        'province_name': [province_name]
+    })
+    
+    # Encode the categorical variables
+    le = joblib.load('label_encoder.joblib')
+    input_df['location'] = le.transform(input_df['location'])
+    input_df['city'] = le.transform(input_df['city'])
+    input_df['province_name'] = le.transform(input_df['province_name'])
+    
+    # Generate polynomial features
+    poly = joblib.load('polynomial_features.joblib')
+    input_df = pd.DataFrame(poly.transform(input_df), columns=poly.get_feature_names(['baths', 'bedrooms', 'area_num']))
+    
+    return input_df
 
-# Create a DataFrame with the user input
-input_df = pd.DataFrame({
-    'property_type': property_type,
-    'location': location,
-    'city': city,
-    'province_name': province_name,
-    'latitude': latitude,
-    'longitude': longitude,
-    'baths': baths,
-    'area': area,
-    'bedrooms': bedrooms
-}, index=[0])
+# Define the Streamlit app
+def app():
+    st.title('Property Price Predictor')
+    
+    # Define the input fields
+    baths = st.number_input('Number of bathrooms', value=2, min_value=1, max_value=10)
+    bedrooms = st.number_input('Number of bedrooms', value=3, min_value=1, max_value=10)
+    area = st.text_input('Area (in square feet)', value='1200')
+    location = st.selectbox('Location', ['DHA Phase 1', 'Bahria Town', 'Gulberg', 'Model Town', 'Johar Town', 'Iqbal Town', 'Township'])
+    city = st.selectbox('City', ['Lahore', 'Islamabad', 'Karachi', 'Rawalpindi', 'Faisalabad', 'Multan', 'Gujranwala', 'Peshawar'])
+    province_name = st.selectbox('Province', ['Punjab', 'Sindh', 'Khyber Pakhtunkhwa', 'Islamabad Capital Territory', 'Balochistan', 'Gilgit-Baltistan', 'Azad Jammu and Kashmir'])
+    
+    # Preprocess the input data
+    input_df = preprocess_input(baths, bedrooms, area, location, city, province_name)
+    
+    # Make the prediction
+    predicted_price = model.predict(input_df)[0]
+    
+    # Display the predicted price
+    st.write('The predicted price of the property is:', predicted_price)
 
-# Make a prediction using the input DataFrame
-prediction = model.predict(input_df)[0]
 
-# Show the prediction
-st.write('The predicted price of this property is $', prediction)
+if __name__ == '__main__':
+    app()
