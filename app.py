237d91998c6abd9a5407d5999c0a5a503e757afe
anarshah <javacodesmaster@gmail.com>
2023-04-07 11:55:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,58 +2,36 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-# Load the trained model
+# Load the model
 model = joblib.load('model.joblib')
 
-# Load the data
-df = pd.read_csv('zameen-property-data.csv')
+# Define the input fields
+st.sidebar.header('Property Information')
+property_type = st.sidebar.selectbox('Property Type', ['House', 'Apartment', 'Condo'])
+location = st.sidebar.text_input('Location')
+city = st.sidebar.selectbox('City', ['Toronto', 'Vancouver', 'Montreal'])
+province_name = st.sidebar.selectbox('Province', ['Ontario', 'British Columbia', 'Quebec'])
+latitude = st.sidebar.number_input('Latitude')
+longitude = st.sidebar.number_input('Longitude')
+baths = st.sidebar.number_input('Number of Bathrooms', value=1)
+area = st.sidebar.number_input('Area (in sqft)', value=500)
+bedrooms = st.sidebar.number_input('Number of Bedrooms', value=1)
 
-# Remove unwanted columns
-df.drop(['property_id', 'location_id', 'page_url', 'purpose', 'date_added', 'agency', 'agent'], axis=1, inplace=True)
+# Create a DataFrame with the user input
+input_df = pd.DataFrame({
+    'property_type': property_type,
+    'location': location,
+    'city': city,
+    'province_name': province_name,
+    'latitude': latitude,
+    'longitude': longitude,
+    'baths': baths,
+    'area': area,
+    'bedrooms': bedrooms
+}, index=[0])
 
-# Convert area column to numeric
-df['area'] = df['area'].str.replace('Marla', '').astype(float)
+# Make a prediction using the input DataFrame
+prediction = model.predict(input_df)[0]
 
-# Convert location column to categorical
-df['location'] = pd.Categorical(df['location']).codes
-
-# Convert city column to categorical
-df['city'] = pd.Categorical(df['city']).codes
-
-# Convert province_name column to categorical
-df['province_name'] = pd.Categorical(df['province_name']).codes
-
-# Convert property_type column to categorical
-df['property_type'] = pd.Categorical(df['property_type']).codes
-
-# Define a function to make a prediction
-def predict_price(bedrooms, bathrooms, area):
-    # Create a dictionary with the user input
-    data = {'bedrooms': bedrooms, 'bathrooms': bathrooms, 'area': area}
-    
-    # Create a DataFrame from the dictionary
-    df_user_input = pd.DataFrame(data, index=[0])
-    
-    # Use the trained model to make a prediction on the user input
-    predicted_price = model.predict(df_user_input)[0]
-    
-    return predicted_price
-
-# Define the Streamlit app
-def app():
-    st.title("Property Price Predictor")
-    
-    # Get user input
-    bedrooms = st.number_input("Enter number of bedrooms", min_value=1, max_value=10, step=1)
-    bathrooms = st.number_input("Enter number of bathrooms", min_value=1, max_value=10, step=1)
-    area = st.number_input("Enter area in square meters", min_value=1.0, max_value=1000.0, step=1.0)
-    
-    # Make a prediction
-    predicted_price = predict_price(bedrooms, bathrooms, area)
-    
-    # Show the predicted price
-    st.write(f"The predicted price is {predicted_price:.2f} PKR.")
-    
-# Run the app
-if __name__ == '__main__':
-    app()
+# Show the prediction
+st.write('The predicted price of this property is $', prediction)
