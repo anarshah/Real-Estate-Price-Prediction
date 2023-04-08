2fc482c1926838a19d8edbbbbee47c237f5cdf9c
anarshah <javacodesmaster@gmail.com>
2023-04-08 14:51:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,22 +22,38 @@ def get_location_options(city):
     return location_options
 
 # define a function to get user inputs and make predictions
-def predict_price(data):
-    # load the trained model
-    model = joblib.load('decision_tree_model.joblib')
-    
-    # encode non-numeric data
-    le = LabelEncoder()
-    for col in data.columns:
-        if data[col].dtype == 'object':
-            data[col] = le.fit_transform(data[col].astype(str))
-    
-    # use the trained model to make a prediction
-    predicted_price = model.predict(data)[0]
-    
-    # return the predicted price
-    return predicted_price
+def predict_price(bedrooms, bathrooms, area, location, city, purpose, property_type):
+    # Load the saved model
+    model = load_model()
 
+    # Load the dataset
+    df = load_data()
+
+    # Remove any rows with missing data
+    df.dropna(inplace=True)
+
+    # Exclude non-numeric columns and columns not needed for prediction
+    numeric_cols = df.select_dtypes(include=np.number).columns.tolist()
+    numeric_cols.remove('property_id') # exclude ID column
+    numeric_cols.remove('location_id') # exclude ID column
+    numeric_cols.remove('latitude') # exclude location column
+    numeric_cols.remove('longitude') # exclude location column
+    numeric_cols.remove('date_added') # exclude date column
+    data = df[numeric_cols]
+
+    # Calculate mean values for imputation
+    mean_values = data.mean()
+
+    # Fill missing values with mean values
+    df.fillna(mean_values, inplace=True)
+
+    # Select subset of data based on input values
+    subset = df[(df['city'] == city) & (df['location'] == location) & (df['area'] == area) & (df['bedrooms'] == bedrooms) & (df['baths'] == bathrooms) & (df['purpose'] == purpose) & (df['property_type'] == property_type)]
+
+    # Make prediction
+    predicted_price = model.predict(subset[numeric_cols])[0]
+
+    return predicted_price
 
 # define the Streamlit app
 def app():
@@ -62,7 +78,12 @@ def app():
     property_type = st.selectbox('Property Type', df['property_type'].unique())
     
     # define input fields for the remaining columns to use as filters
-    price = st.slider('Price (in PKR)', min_value=float(df['price'].min()), max_value=float(df['price'].max()), step=100000.0, value=float(df['price'].mean()))
+    price = st.slider('Price (in PKR)', 
+                  min_value=int(df['price'].min()), 
+                  max_value=int(df['price'].max()), 
+                  step=100000, 
+                  value=int(df['price'].mean()))
+
     baths = st.slider('Number of Baths', min_value=int(df['baths'].min()), max_value=int(df['baths'].max()), step=1, value=int(df['baths'].mean()))
     # drop rows with non-numeric values in "date_added" column
     df = df[pd.to_numeric(df['date_added'], errors='coerce').notnull()]
