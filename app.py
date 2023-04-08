f90a8419db805ae0a011c6e8fedf396508c8b0af
anarshah <javacodesmaster@gmail.com>
2023-04-08 11:04:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,36 @@ model = joblib.load('decision_tree_model.joblib')
 
 # Create a function to preprocess user input and make predictions
 def predict_price(location, area, bedrooms, bathrooms):
-    # Load the data and encode the location column
-    data = pd.DataFrame({
+    # Load the data and select the necessary columns
+    data = pd.read_csv('zameen-property-data.csv')[['location', 'area', 'bedrooms', 'baths', 'price']]
+    data['location'] = le.transform(data['location'])
+    
+    # Create a new DataFrame with the user input
+    user_input = pd.DataFrame({
         'location': [location],
         'area': [area],
         'bedrooms': [bedrooms],
-        'bathrooms': [bathrooms]
+        'baths': [bathrooms]
     })
-    data['location'] = le.transform(data['location'])
+    user_input['location'] = le.transform(user_input['location'])
+    
+    # Merge the user input with the original data and select the necessary columns
+    data = pd.concat([data, user_input])
+    data = data[['location', 'area', 'bedrooms', 'baths']]
+    
+    # Separate the user input from the original data
+    X = data.iloc[:-1]
+    user_input = data.iloc[-1:]
+    
+    # Train a new label encoder on the 'location' column of the updated data
+    le_new = LabelEncoder()
+    le_new.fit(data['location'])
+    
+    # Encode the 'location' column of the user input using the new label encoder
+    user_input['location'] = le_new.transform(user_input['location'])
     
     # Make a prediction using the model
-    price = model.predict(data)[0]
+    price = model.predict(user_input)[0]
     return price
 
 # Create a Streamlit app
