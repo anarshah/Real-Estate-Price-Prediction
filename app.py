91adf3f23be1ae916d6f92f75bf85aae4b4f6f2c
anarshah <javacodesmaster@gmail.com>
2023-04-08 12:25:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,42 @@
+import pandas as pd
 import streamlit as st
 import joblib
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
-# Load the label encoder and decision tree model
-le = joblib.load('label_encoder.joblib')
+# load the trained model
 model = joblib.load('decision_tree_model.joblib')
 
-# Create a function to preprocess user input and make predictions
-def predict_price(location, area, bedrooms, bathrooms):
-    # Load the data and select the necessary columns
-    data = pd.read_csv('zameen-property-data.csv')[['location', 'area', 'bedrooms', 'baths', 'price']]
-    data['location'] = le.transform(data['location'])
-    
-    # Create a new DataFrame with the user input
-    user_input = pd.DataFrame({
-        'location': [location],
-        'area': [area],
-        'bedrooms': [bedrooms],
-        'baths': [bathrooms]
-    })
-    user_input['location'] = le.transform(user_input['location'])
-    
-    # Merge the user input with the original data and select the necessary columns
-    data = pd.concat([data, user_input])
-    data = data[['location', 'area', 'bedrooms', 'baths']]
-    
-    # Separate the user input from the original data
-    X = data.iloc[:-1]
-    user_input = data.iloc[-1:]
-    
-    # Train a new label encoder on the 'location' column of the updated data
-    le_new = LabelEncoder()
-    le_new.fit(data['location'])
-    
-    # Encode the 'location' column of the user input using the new label encoder
-    user_input['location'] = le_new.transform(user_input['location'])
-    
-    # Make a prediction using the model
-    price = model.predict(user_input)[0]
-    return price
+# define the columns used to train the model
+columns = ['bedrooms', 'bathrooms', 'area', 'location']
 
-# Create a Streamlit app
-st.title("Property Price Predictor")
+# define a function to get user inputs and make predictions
+def predict_price(bedrooms, bathrooms, area, location):
+    # create a DataFrame with the user inputs
+    data = pd.DataFrame([[bedrooms, bathrooms, area, location]], columns=columns)
+    
+    # use the trained model to make a prediction
+    predicted_price = model.predict(data)[0]
+    
+    # return the predicted price
+    return predicted_price
 
-# Create input fields for user input
-location = st.selectbox("Location", options=pd.read_csv('zameen-property-data.csv')['location'].unique())
-area = st.number_input("Area (in Marla)")
-bedrooms = st.number_input("Bedrooms")
-bathrooms = st.number_input("Bathrooms")
+# define the Streamlit app
+def app():
+    st.title('Zameen Property Price Predictor')
+    
+    # define input fields for the user to enter data
+    bedrooms = st.number_input('Number of Bedrooms')
+    bathrooms = st.number_input('Number of Bathrooms')
+    area = st.number_input('Area (in square feet)')
+    location = st.text_input('Location')
+    
+    # define a button to trigger the prediction
+    if st.button('Predict Price'):
+        # make a prediction using the user inputs
+        predicted_price = predict_price(bedrooms, bathrooms, area, location)
+        
+        # display the predicted price to the user
+        st.success(f'Predicted Price: {predicted_price:.2f} PKR')
 
-# Create a button to make the prediction
-if st.button("Predict Price"):
-    price = predict_price(location, area, bedrooms, bathrooms)
-    st.write("The predicted price is:", price)
+# run the Streamlit app
+if __name__ == '__main__':
+    app()
