4b0b562633fdcfd27d62dbd107b9dee504e905da
anarshah <javacodesmaster@gmail.com>
2023-04-08 10:38:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,22 +12,34 @@ model = joblib.load('decision_tree_model.joblib')
 le = joblib.load('label_encoder.joblib')
 
 def predict_price(model, city, area_sqft, bedrooms, baths):
-    # Encode the city using the same LabelEncoder used during training
+    # Encode the location, city, and area using the label encoder object
+    location_encoded = le.transform([location])[0]
     city_encoded = le.transform([city])[0]
+    area_encoded = le.transform([area])[0]
 
     # Create a new DataFrame with the same columns as your training data
-    input_data = pd.DataFrame(columns=X_train.columns[:4]) # use only the first 4 columns
-    
+    input_data = pd.DataFrame(columns=X.columns)
+
     # Fill in the user's input data
+    input_data.loc[0, 'location'] = location_encoded
     input_data.loc[0, 'city'] = city_encoded
-    input_data.loc[0, 'area'] = area_sqft
+    input_data.loc[0, 'area'] = area_encoded
     input_data.loc[0, 'bedrooms'] = bedrooms
     input_data.loc[0, 'baths'] = baths
 
+    # Set other columns to their mean or mode values, as appropriate
+    for col in input_data.columns:
+        if col not in ['location', 'city', 'area', 'bedrooms', 'baths']:
+            if input_data[col].dtype == 'object':
+                input_data[col] = df[col].mode().iloc[0]
+            else:
+                input_data[col] = df[col].mean()
+
     # Make a prediction using the trained model
     price_prediction = model.predict(input_data)
-    
+
     return price_prediction[0]
+
 
 # Set up the Streamlit app
 st.title("Property Price Predictor")
