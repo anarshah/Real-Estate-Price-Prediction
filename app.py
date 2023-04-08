3548bea6da03f0446eee07f70c6681b775ba13f4
anarshah <javacodesmaster@gmail.com>
2023-04-08 14:23:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def get_location_options(city):
     return location_options
 
 # define a function to get user inputs and make predictions
-def predict_price(bedrooms, bathrooms, area, location, city, purpose, property_type):
-    # create a DataFrame with the user inputs
-    data = pd.DataFrame([[bedrooms, bathrooms, area, location, city, purpose, property_type]], columns=columns)
+def predict_price(data):
+    # load the trained model
+    model = joblib.load('decision_tree_model.joblib')
     
     # encode non-numeric data
     le = LabelEncoder()
@@ -38,40 +38,63 @@ def predict_price(bedrooms, bathrooms, area, location, city, purpose, property_t
     # return the predicted price
     return predicted_price
 
+
 # define the Streamlit app
 def app():
     st.title('Zameen Property Price Predictor')
     
-    # define input fields for the user to enter data
-    bedrooms = st.number_input('Number of Bedrooms')
-    bathrooms = st.number_input('Number of Bathrooms')
-    area = st.number_input('Area (in square feet or marla)')
+    # load the dataset
+    df = pd.read_csv('zameen-property-data.csv')
     
-    # define a dropdown menu for the "city" input field
-    city = st.selectbox('City', city_options)
+    # define input fields for the user to enter data
+    city = st.selectbox('City', df['city'].unique())
     
     # get unique values for the "location" column based on the selected city
-    location_options = get_location_options(city)
+    location_options = df[df['city'] == city]['location'].unique()
     
-    # define a dropdown menu for the "location" input field
     location = st.selectbox('Location', location_options)
     
-    # define a dropdown menu for the "purpose" input field
-    purpose = st.selectbox('Purpose', purpose_options)
+    area = st.number_input('Area (in square feet)')
+    bedrooms = st.number_input('Number of Bedrooms')
+    bathrooms = st.number_input('Number of Bathrooms')
     
-    # define a dropdown menu for the "property type" input field
+    purpose = st.selectbox('Purpose', df['purpose'].unique())
     property_type = st.selectbox('Property Type', df['property_type'].unique())
+    
+    # define input fields for the remaining columns to use as filters
+    price = st.slider('Price (in PKR)', min_value=df['price'].min(), max_value=df['price'].max(), step=100000, value=df['price'].mean())
+    baths = st.slider('Number of Baths', min_value=df['baths'].min(), max_value=df['baths'].max(), step=1, value=df['baths'].mean())
+    date_added = st.date_input('Date Added', df['date_added'].min(), df['date_added'].max(), df['date_added'].mean())
     
     # define a button to trigger the prediction
     if st.button('Predict Price'):
-        # filter the dataset based on the user inputs
-        filtered_data = df[(df['city'] == city) & (df['location'] == location) & (df['bedrooms'] == bedrooms) & (df['baths'] == bathrooms) & (df['purpose'] == purpose) & (df['property_type'] == property_type)]
+        # create a DataFrame with the user inputs
+        data = pd.DataFrame({
+            'city': [city],
+            'location': [location],
+            'area': [area],
+            'bedrooms': [bedrooms],
+            'baths': [bathrooms],
+            'purpose': [purpose],
+            'property_type': [property_type],
+            'price': [price],
+            'date_added': [date_added],
+            'property_id': [0],
+            'location_id': [0],
+            'page_url': [''],
+            'province_name': [''],
+            'latitude': [0.0],
+            'longitude': [0.0],
+            'agency': [''],
+            'agent': ['']
+        })
         
-        # use the filtered dataset to make a prediction
-        predicted_price = predict_price(bedrooms, bathrooms, area, location, city, purpose, property_type)
+        # make a prediction using the user inputs
+        predicted_price = predict_price(data)
         
         # display the predicted price to the user
         st.success(f'Predicted Price: {predicted_price:.2f} PKR')
+
 
 # run the Streamlit app
 if __name__ == '__main__':
