418a3805fcd366819010d060a3d987ef82e09bdc
anarshah <javacodesmaster@gmail.com>
2023-04-09 15:47:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_user_inputs():
         "city": [city],
         "province_name": [province_name],
         "bedrooms": [bedrooms],
-        "bathrooms": [bathrooms],
+        "baths": [baths],
         "area": [area],
         "purpose": [purpose],
     })
