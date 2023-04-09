30d1ecdb8b2e2937d4d02302a71b8e937bb44d7f
anarshah <javacodesmaster@gmail.com>
2023-04-09 16:11:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ import pandas as pd
 # Load the trained model
 model = joblib.load("zameen_property_model.joblib")
 
+# preprocess_area function definition
+def preprocess_area(area_str):
+    
+    def marla_to_sqft(marla):
+        return marla * 225
+
+    def kanal_to_sqft(kanal):
+        return kanal * 5062.5
+
+    area_value, area_unit = area_str.split()
+    area_value = float(area_value.replace(",", ""))
+
+    if area_unit.lower() == "marla":
+        area_value = marla_to_sqft(area_value)
+    elif area_unit.lower() == "kanal":
+        area_value = kanal_to_sqft(area_value)
+    # Add more units if needed
+
+    return area_value
 # Load the data for province, city, and location options
 data = pd.read_csv("zameen-property-data.csv")
 provinces = data["province_name"].unique()
