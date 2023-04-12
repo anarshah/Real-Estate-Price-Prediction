46b4f0ecccc0decb84e3391651b009b6486bd754
anarshah <javacodesmaster@gmail.com>
2023-04-12 10:35:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,47 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
 from joblib import load
+from sklearn.tree import DecisionTreeRegressor
 
-# Load the trained model
+# Load the saved model
 dtr = load('property_predictor.joblib')
 
-# Load preprocessed data
-df = pd.read_csv('property_updated_csv.csv')
+# Read the preprocessed data
+data = pd.read_csv('property_updated_csv.csv')
 
-# Define the function to make predictions
+# Prepare the feature matrix X and target variable y
+X = data.drop(['price', 'price_per_sqft', 'city_location'], axis=1)
+y = data['price']
+
 def predict_price(city, location, sqft, bedrooms, baths):
     city_location = city + '_' + location
-    X = pd.get_dummies(df.drop("price", axis=1))
-
-    if city_location in X.columns:
-        loc_index = np.where(X.columns == city_location)[0][0]
-    else:
-        loc_index = -1
+    if city_location not in data['city_location'].unique():
+        print(f"City location '{city_location}' not found in the dataset.")
+        return None
 
     x = np.zeros(len(X.columns))
     x[0] = baths
     x[1] = sqft
     x[2] = bedrooms
-    if loc_index >= 0:
-        x[loc_index] = 1
-    return dtr.predict([x])[0] / 100000
+    
+    city_location_indices = data[data['city_location'] == city_location].index
+    X_city_location = X.loc[city_location_indices]
+    X_city_location = X_city_location.reset_index(drop=True)
 
-# Define Streamlit app
+    return dtr.predict([x])[0]
+
 st.title('Real Estate Price Prediction')
 
-city = st.selectbox('City', df['city_location'].unique())
-sqft = st.number_input('Square Feet', min_value=300, step=10)
-bedrooms = st.number_input('Bedrooms', min_value=1, step=1)
-baths = st.number_input('Bathrooms', min_value=1, step=1)
+city = st.selectbox('City', sorted(data['city_location'].str.split('_').str[0].unique()))
+location = st.selectbox('Location', sorted(data[data['city_location'].str.startswith(city)]['city_location'].str.split('_').str[1].unique()))
+sqft = st.number_input('Area in Square Feet', min_value=1, max_value=int(data['area'].max()), step=1)
+bedrooms = st.number_input('Number of Bedrooms', min_value=1, max_value=int(data['bedrooms'].max()), step=1)
+baths = st.number_input('Number of Baths', min_value=1, max_value=int(data['baths'].max()), step=1)
 
 if st.button('Predict Price'):
     predicted_price = predict_price(city, location, sqft, bedrooms, baths)
-    st.write(f"Predicted Price: PKR {predicted_price} Lakhs")
+    if predicted_price:
+        st.success(f"Predicted price: {predicted_price:.2f} PKR")
+    else:
+        st.error("Unable to make a prediction for the given city and location.")
