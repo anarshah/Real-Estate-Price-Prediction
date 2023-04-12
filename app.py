7dacbfe2dcb1e916d46fdf824c135ccea0d563c2
anarshah <javacodesmaster@gmail.com>
2023-04-12 10:24:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 from joblib import load
 
-# Load preprocessed data and trained model
-df = pd.read_csv('property_updated_csv.csv')
+# Load the trained model
 dtr = load('property_predictor.joblib')
 
-# Get list of unique city_location values
-city_locations = df['city_location'].unique()
+# Load preprocessed data
+df = pd.read_csv('property_updated_csv.csv')
 
-# Function to predict price
-def predict_price(city_location, sqft, bedrooms, baths):
-    x = pd.DataFrame(columns=X.columns)
-    x.loc[0] = np.zeros(len(X.columns))
-    x.loc[0, 'baths'] = baths
-    x.loc[0, 'area'] = sqft
-    x.loc[0, 'bedrooms'] = bedrooms
-    if city_location in x.columns:
-        x.loc[0, city_location] = 1
-    return dtr.predict(x.values)[0] / 100000
+# Define the function to make predictions
+def predict_price(city, location, sqft, bedrooms, baths):
+    city_location = city + '_' + location
+    X = pd.get_dummies(df.drop("price", axis=1))
 
-# Create X dataframe for prediction function
-X = df.drop(['price', 'price_per_sqft'], axis=1)
+    if city_location in X.columns:
+        loc_index = np.where(X.columns == city_location)[0][0]
+    else:
+        loc_index = -1
 
-# Streamlit app title
-st.title("Property Price Predictor")
+    x = np.zeros(len(X.columns))
+    x[0] = baths
+    x[1] = sqft
+    x[2] = bedrooms
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return dtr.predict([x])[0] / 100000
 
-# User input fields
-city_location = st.selectbox("City and Location", city_locations)
-sqft = st.number_input("Area (in square feet)", value=1000)
-bedrooms = st.slider("Number of Bedrooms", min_value=1, max_value=10, value=2)
-baths = st.slider("Number of Bathrooms", min_value=1, max_value=10, value=2)
+# Define Streamlit app
+st.title('Real Estate Price Prediction')
 
-# Predict button
-if st.button("Predict Price"):
-    predicted_price = predict_price(city_location, sqft, bedrooms, baths)
-    st.write("The estimated price is {:.2f} Lakhs".format(predicted_price))
+city = st.selectbox('City', df['city'].unique())
+location = st.selectbox('Location', df['location'].unique())
+sqft = st.number_input('Square Feet', min_value=300, step=10)
+bedrooms = st.number_input('Bedrooms', min_value=1, step=1)
+baths = st.number_input('Bathrooms', min_value=1, step=1)
+
+if st.button('Predict Price'):
+    predicted_price = predict_price(city, location, sqft, bedrooms, baths)
+    st.write(f"Predicted Price: PKR {predicted_price} Lakhs")
